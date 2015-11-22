2aeea63413ad7ca6161908fb51b84be763c3aaf6
Artur Kaleta <49493036+Abuseware@users.noreply.github.com>
2015-11-22 13:18:37 +0100

    Formatting

--- ringbuffer.c
+++ ringbuffer.c
@@ -2,45 +2,45 @@
 
 #include "ringbuffer.h"
 
-ring_buffer_t* ring_buffer_create(uint16_t size){
-    ring_buffer_t *ring = (ring_buffer_t*)malloc(sizeof(ring_buffer_t));
-    ring->buffer = malloc(size);
-    ring->size = size;
-    ring->start = 0;
-    ring->end = 0;
+ring_buffer_t* ring_buffer_create(uint16_t size) {
+	ring_buffer_t *ring = (ring_buffer_t*)malloc(sizeof(ring_buffer_t));
+	ring->buffer = malloc(size);
+	ring->size = size;
+	ring->start = 0;
+	ring->end = 0;
 
-    return ring;
+	return ring;
 }
 
-void ring_buffer_destroy(ring_buffer_t* ring){
-    if(ring){
-        free(ring->buffer);
-        free(ring);
-    }
+void ring_buffer_destroy(ring_buffer_t* ring) {
+	if(ring) {
+		free(ring->buffer);
+		free(ring);
+	}
 }
 
-uint8_t ring_buffer_write(ring_buffer_t* ring, uint8_t data){
-    uint8_t next = (ring->end + 1) % ring->size;
+uint8_t ring_buffer_write(ring_buffer_t* ring, uint8_t data) {
+  uint8_t next = (ring->end + 1) % ring->size;
 
-    if(next == ring->start){
-        return 1;
-    }
+  if(next == ring->start) {
+    return 1;
+  }
 
-    ring->buffer[ring->end] = data;
+  ring->buffer[ring->end] = data;
 
-    ring->end = next;
+  ring->end = next;
 
-    return 0;
+  return 0;
 }
 
-uint8_t ring_buffer_read(ring_buffer_t* ring, uint8_t* target){
-    if(ring->end == ring->start){
-        return 1;
-    }
+uint8_t ring_buffer_read(ring_buffer_t* ring, uint8_t* target) {
+  if(ring->end == ring->start) {
+    return 1;
+  }
 
-    *target = ring->buffer[ring->start];
+  *target = ring->buffer[ring->start];
 
-    ring->start = (ring->start + 1) % ring->size;
+  ring->start = (ring->start + 1) % ring->size;
 
-    return 0;
+  return 0;
 }
